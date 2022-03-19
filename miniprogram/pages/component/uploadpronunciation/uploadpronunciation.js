ae638999ce6f4d93ffda2733342d66a17b9d4404
Spider <2464831035@qq.com>
2022-03-20 02:19:58 +0800

    improve uploading pronunciation

--- miniprogram/pages/component/uploadpronunciation/uploadpronunciation.js
+++ miniprogram/pages/component/uploadpronunciation/uploadpronunciation.js
@@ -1,18 +1,47 @@
 const app = getApp()
+const countys = ['城厢区', '涵江区', '荔城区', '秀屿区', '仙游县']
+const towns = [
+  ['龙桥街道', '凤凰山街道', '霞林街道', '常太镇', '华亭镇', '灵川镇', '东海镇'],
+  ['涵东街道', '涵西街道', '三江口镇', '白塘镇', '国欢镇', '梧塘镇', '江口镇', '萩芦镇', '白沙镇', '庄边镇', '新县镇', '大洋乡'],
+  ['镇海街道', '拱辰街道', '西天尾镇', '黄石镇', '新度镇', '北高镇'],
+  ['笏石镇', '东庄镇', '忠门镇', '东埔镇', '东峤镇', '埭头镇', '平海镇', '南日镇', '湄洲镇', '山亭镇', '月塘乡'],
+  ['鲤城街道', '枫亭镇', '榜头镇', '郊尾镇', '度尾镇', '鲤南镇', '赖店镇', '盖尾镇', '园庄镇', '大济镇', '龙华镇', '钟山镇', '游洋镇', '西苑乡', '石苍乡', '社硎乡', '书峰乡', '菜溪乡']
+]
 
 Page({
   data: {
     id: 0,
     word: '',
+    ipa: '',
+    pinyin: '',
     status: 0,
-    source: ''
+    source: '',
+    multiIndex: [0, 0],
+    multiArray: [
+      countys,
+      towns[0]
+    ]
   },
 
   onLoad(options) {
     this.setData({
       id: options.id,
-      word: options.word
+      word: options.word,
+      ipa: options.ipa,
+      pinyin: options.pinyin
     })
+
+    if (app.globalData.userInfo.county) {
+      let index_0 = countys.indexOf(app.globalData.userInfo.county)
+      let index_1 = towns[index_0].indexOf(app.globalData.userInfo.town)
+      let multiArray = this.data.multiArray
+      multiArray[1] = towns[index_0]
+      this.setData({
+        multiIndex: [index_0, index_1],
+        multiArray: multiArray
+      })
+    }
+
     var that = this;
     // 创建录音管理器
     this.recorderManager = wx.getRecorderManager()
@@ -29,25 +58,43 @@ Page({
     })
   },
 
-  recordOrOver() {
-    // 未在录音
-    if (this.data.status == 0) {
-      this.setData({
-        status: 1
-      })
-      // 开始录音
-      this.recorderManager.start({
-        format: 'mp3'
-      })
-    }
-    // 正在录音
-    else if (this.data.status == 1) {
-      this.setData({
-        status: 0
-      })
-      // 结束录音
-      this.recorderManager.stop()
-    }
+  // recordOrOver() {
+  //   // 未在录音
+  //   if (this.data.status == 0) {
+  //     this.setData({
+  //       status: 1
+  //     })
+  //     // 开始录音
+  //     this.recorderManager.start({
+  //       format: 'mp3'
+  //     })
+  //   }
+  //   // 正在录音
+  //   else if (this.data.status == 1) {
+  //     this.setData({
+  //       status: 0
+  //     })
+  //     // 结束录音
+  //     this.recorderManager.stop()
+  //   }
+  // },
+
+  startRecord() {
+    this.setData({
+      status: 1
+    })
+    // 开始录音
+    this.recorderManager.start({
+      format: 'mp3'
+    })
+  },
+
+  stopRecord() {
+    this.setData({
+      status: 0
+    })
+    // 结束录音
+    this.recorderManager.stop()
   },
 
   // 上传.mp3文件
@@ -78,24 +125,65 @@ Page({
     var src = this.data.source;
     if (src == '') {
       wx.showToast({
-        title: '请先录音！',
+        title: '请先录音',
         icon: 'error'
       })
       return;
+    } else {
+      wx.showToast({
+        title: '正在播放录音...',
+        icon: 'none'
+      })
     }
     this.innerAudioContext.src = src;
     this.innerAudioContext.play()
   },
 
+  multiChange(e) {
+    this.setData({
+      multiIndex: e.detail.value
+    })
+  },
+
+  columnChange(e) {
+    let data = {
+      multiArray: this.data.multiArray,
+      multiIndex: this.data.multiIndex
+    }
+    data.multiIndex[e.detail.column] = e.detail.value
+    if (e.detail.column == 0) {
+      switch (data.multiIndex[0]) {
+        case 0:
+          data.multiArray[1] = towns[0]
+          break;
+        case 1:
+          data.multiArray[1] = towns[1]
+          break;
+        case 2:
+          data.multiArray[1] = towns[2]
+          break;
+        case 3:
+          data.multiArray[1] = towns[3]
+          break;
+        case 4:
+          data.multiArray[1] = towns[4]
+          break;
+      }
+      this.setData(data)
+    }
+  },
+
   release(e) {
     let pronunciation = {
       word: this.data.id,
-      source: e.detail.value.source,
+      source: this.data.source,
       ipa: e.detail.value.ipa,
       pinyin: e.detail.value.pinyin,
-      county: e.detail.value.county,
-      town: e.detail.value.town
+      county: countys[this.data.multiIndex[0]],
+      town: towns[this.data.multiIndex[0]][this.data.multiIndex[1]]
     }
+
+    // 发起网络请求
     wx.request({
       url: app.globalData.server + 'pronunciation',
       method: 'POST',
